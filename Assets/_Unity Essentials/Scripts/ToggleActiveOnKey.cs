d62d7037d87b83697cd5c5a64967905b4f7a4a76
aprotasenya <alexzander.protasenya@gmail.com>
2024-09-24 00:05:30 +0300

    ToggleActiveOnKey script

---
+++ Assets/_Unity Essentials/Scripts/ToggleActiveOnKey.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleActiveOnKey : MonoBehaviour
+{
+    [SerializeField] private KeyCode toggleKey = KeyCode.H;
+    [SerializeField] private GameObject[] startOn;
+    [SerializeField] private GameObject[] startOff;
+    private bool stateSwitch = true;
+
+
+    void Start()
+    {
+        ArraySetActive(startOn, active: true);
+        ArraySetActive(startOff, active: false);
+
+    }
+
+    private void ArraySetActive(GameObject[] array, bool active)
+    {
+        foreach (var item in array)
+        {
+            item.SetActive(active);
+        }
+    }
+
+    private void ArrayToggleActive(GameObject[] array)
+    {
+        foreach (var item in array)
+        {
+            item.SetActive(!item.activeSelf);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(toggleKey)) {
+            ArrayToggleActive(startOn);
+            ArrayToggleActive(startOff);
+        }
+    }
+}
