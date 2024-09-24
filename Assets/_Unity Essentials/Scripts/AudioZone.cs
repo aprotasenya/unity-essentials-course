316237649394d748bbc5f530104f0192be7420e8
aprotasenya <alexzander.protasenya@gmail.com>
2024-09-24 12:04:23 +0300

    audio zones across the scene

---
+++ Assets/_Unity Essentials/Scripts/AudioZone.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioZone : MonoBehaviour
+{
+    [SerializeField] private AudioClip music;
+    [SerializeField] private AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (source == null) { GetComponentInParent<AudioSource>(); }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>() != null)
+        {
+            ChangeMusic();
+        }
+    }
+
+    private void ChangeMusic()
+    {
+        if (source.clip == music) { return; }
+
+        source.Stop();
+        source.clip = music;
+        source.Play();
+    }
+}
