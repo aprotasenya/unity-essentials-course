8d4d289551caa896bbec27137e2ff5c7ffe4999e
aprotasenya <alexzander.protasenya@gmail.com>
2024-10-16 15:18:16 +0300

    VFX to SelfDestruct

---
+++ Assets/_Unity Essentials/Scripts/SelfdestructOnTimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfdestructOnTimer : MonoBehaviour
+{
+    [SerializeField] float selfdestructAfter = 1f;
+    float currentTime = 0;
+
+    void Start()
+    {
+        currentTime = selfdestructAfter;
+    }
+
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0) SelfDesctruct();
+    }
+
+    private void SelfDesctruct()
+    {
+        Destroy(gameObject);
+    }
+}
